d1e2a269e8a7c31d87a591b1a49a91fe122d491e
dannypsnl <dannypsnl@gmail.com>
2020-04-06 04:41:09 +0800

    buy me a coffee button

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -32,6 +32,21 @@ export default ({ data }) => {
             <ShowTag>{tag}</ShowTag>
           ))}
         </FooterSection>
+        <SiteLink
+          className="bmc-button"
+          url="https://www.buymeacoffee.com/zclIv4D"
+        >
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/default-white.png"
+            alt="Buy Me A Coffee"
+            style={{
+              height: `51px`,
+              width: `217px`,
+              marginTop: `1em`,
+              marginBottom: `0`,
+            }}
+          />
+        </SiteLink>
         <Disqus config={disqusConfig} />
       </div>
     </Layout>
