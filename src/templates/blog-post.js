cd8543d54316361222db4f489a46f2982a9f113e
dannypsnl <dannypsnl@gmail.com>
2020-04-06 03:59:19 +0800

    (#17) show author, categories, tag in post page
    resolve #17

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import { Disqus } from "gatsby-plugin-disqus"
+import SiteLink from "../components/site-link"
 
 export default ({ data }) => {
   const post = data.markdownRemark
@@ -15,11 +16,51 @@ export default ({ data }) => {
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <FooterSection>
+          author:{" "}
+          <SiteLink url={`https://github.com/dannypsnl`}>dannypsnl</SiteLink>
+        </FooterSection>
+        <FooterSection>
+          category:
+          {post.frontmatter.categories.map((category) => (
+            <ShowTag>{category}</ShowTag>
+          ))}
+        </FooterSection>
+        <FooterSection>
+          tag:
+          {post.frontmatter.tags.map((tag) => (
+            <ShowTag>{tag}</ShowTag>
+          ))}
+        </FooterSection>
         <Disqus config={disqusConfig} />
       </div>
     </Layout>
   )
 }
+
+const FooterSection = ({ children }) => (
+  <p
+    style={{
+      marginBottom: `0`,
+    }}
+  >
+    {children}
+  </p>
+)
+
+const ShowTag = ({ children }) => (
+  <em
+    style={{
+      margin: `0.2em`,
+      padding: `0.1em`,
+      color: `#aba4a4`,
+      border: `solid 0.3px`,
+      textAlign: `center`,
+    }}
+  >
+    {children}
+  </em>
+)
 
 export const query = graphql`
   query($slug: String!) {
@@ -32,6 +73,8 @@ export const query = graphql`
       html
       frontmatter {
         title
+        categories
+        tags
       }
     }
   }
