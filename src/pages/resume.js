f0c26b83a2b79d2acadfcc9c8c02e763490c4335
dannypsnl <dannypsnl@gmail.com>
2020-04-14 14:18:59 +0800

    formatting

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -99,9 +99,7 @@ export default ({ data }) => {
           <div>
             <b>Sky Mirror(software engineer)</b>{" "}
             <SiteLink url={`https://www.skymirror.com.tw/`} />
-            <p>
-              Maintaining a future trading system.
-            </p>
+            <p>Maintaining a future trading system.</p>
           </div>
           <div>
             <b>Glasnostic(system software engineer)</b>{" "}
