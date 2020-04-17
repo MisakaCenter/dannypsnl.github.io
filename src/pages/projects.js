988cd6ece23d8cf30ea472c76e393c6918404155
dannypsnl <dannypsnl@gmail.com>
2020-04-18 01:14:02 +0800

    restructure projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,67 +4,114 @@ import SEO from "../components/seo"
 import SiteLink from "../components/site-link"
 import { graphql } from "gatsby"
 
-export default ({ data }) => (
-  <Layout>
-    <SEO title={`Projects`} description={data.site.siteMetadata.title} />
-    <div>
-      <h1
-        style={{
-          textAlign: `center`,
-        }}
-      >
-        Projects
-      </h1>
+export default ({ data }) => {
+  const code_projects = [
+    {
+      link: `https://github.com/dannypsnl/plt-researchs`,
+      name: `plt-researches`,
+      description: `A collection of PLT researching`,
+    },
+    {
+      link: `https://github.com/dannypsnl/elz`,
+      name: `elz`,
+      description: `A polymorphism object-oriented programming language`,
+    },
+    {
+      link: `https://github.com/dannypsnl/xnix`,
+      name: `xnix`,
+      description: `A unix-like system setting`,
+    },
+    {
+      link: `https://github.com/dannypsnl/little-scheme`,
+      name: `little-scheme`,
+      description: `A little scheme for playing SICP`,
+    },
+    {
+      link: `https://github.com/dannypsnl/redux`,
+      name: `redux`,
+      description: `redux in Go/Rust`,
+    },
+    {
+      link: `https://github.com/dannypsnl/rocket`,
+      name: `rocket`,
+      description: `A light weight Go web framework`,
+    },
+    {
+      link: `https://github.com/dannypsnl/on`,
+      name: `on`,
+      description: `A command line tool execute commands base on a command context to avoid duplicate typing`,
+    },
+  ]
 
-      <h3>Code</h3>
-      <ul>
-        <li>
-          <SiteLink url={`https://github.com/dannypsnl/elz`}>elz</SiteLink>
-        </li>
-        <li>
-          <SiteLink url={`https://github.com/dannypsnl/little-scheme`}>
-            little-scheme
-          </SiteLink>
-        </li>
-        <li>
-          <SiteLink url={`https://github.com/dannypsnl/rocket`}>
-            rocket
-          </SiteLink>
-          <b>: Light Go web framework</b>
-        </li>
-        <li>
-          <SiteLink url={`https://github.com/dannypsnl/redux`}>
-            redux-go
-          </SiteLink>
-          <b>: redux in Go</b>
-        </li>
-      </ul>
-      <h4>Contribution</h4>
-      <ul>
-        <li>
-          <SiteLink url={`https://github.com/llir/llvm`}>llir/llvm</SiteLink>
-        </li>
-      </ul>
+  const contributions = [
+    {
+      link: `https://github.com/llir/llvm`,
+      name: `llir/llvm`,
+      description: `Library for interacting with LLVM IR in pure Go`,
+    },
+  ]
 
-      <h3>NOTE</h3>
-      <ul>
-        <li>
-          <SiteLink
-            url={`https://dannypsnl.github.io/plt-researchs/the-little-typer-note/note.html`}
-          >
-            NOTE: the little typer
-          </SiteLink>
-        </li>
-        <li>
-          <SiteLink
-            url={`https://dannypsnl.github.io/plt-researchs/stlc/stlc.html`}
-          >
-            STLC
-          </SiteLink>
-        </li>
-      </ul>
-    </div>
-  </Layout>
+  const notes = [
+    {
+      link: `https://dannypsnl.github.io/plt-researchs/the-little-typer-note/note.html`,
+      name: `NOTE: the little typer`,
+      description: `Note for a book <<The little typer>>`,
+    },
+    {
+      link: `https://dannypsnl.github.io/plt-researchs/stlc/stlc.html`,
+      name: `STLC`,
+      description: `Simply typed lambda calculus formula`,
+    },
+  ]
+
+  return (
+    <Layout>
+      <SEO title={`Projects`} description={data.site.siteMetadata.title} />
+      <div>
+        <h1
+          style={{
+            textAlign: `center`,
+          }}
+        >
+          Projects
+        </h1>
+
+        <h3>Code</h3>
+        <ul>
+          {code_projects.map((project) => (
+            <ProjectLi project={project} />
+          ))}
+        </ul>
+        <h4>Contribution</h4>
+        <ul>
+          {contributions.map((contribution) => (
+            <ProjectLi project={contribution} />
+          ))}
+        </ul>
+
+        <h3>NOTE</h3>
+        <ul>
+          {notes.map((note) => (
+            <ProjectLi project={note} />
+          ))}
+        </ul>
+      </div>
+    </Layout>
+  )
+}
+
+const ProjectLi = ({ project }) => (
+  <li>
+    <SiteLink url={project.link}>{project.name}</SiteLink>
+    {" : "}
+    <em
+      style={{
+        fontSize: `0.9em`,
+      }}
+    >
+      {project.description}
+    </em>
+  </li>
 )
 
 export const query = graphql`
