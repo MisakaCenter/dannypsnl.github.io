9613f9a34f7245aba12487088ddb5cef03fad6df
dannypsnl <dannypsnl@gmail.com>
2020-04-18 01:49:25 +0800

    extract out license part as component

--- src/components/layout.js
+++ src/components/layout.js
@@ -32,31 +32,34 @@ const Layout = ({ children }) => {
         <footer>
           ©{new Date().getFullYear()}{" "}
           <SiteLink url={`https://github.com/dannypsnl`}>dannypsnl</SiteLink>
-          <div>
-            <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
-              <img
-                alt="Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
-              International"
-                style={{
-                  float: `left`,
-                  margin: `0.5em`,
-                  borderWidth: `0`,
-                }}
-                src="https://i.creativecommons.org/l/by/4.0/88x31.png"
-              />
-            </a>
-            All works in this site is licensed under a{" "}
-            <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
-              Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
-              International
-            </a>{" "}
-            License.
-          </div>
+          <LicenseCC4 />
         </footer>
       </div>
     </>
   )
 }
+
+const LicenseCC4 = () => (
+  <div>
+    <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+      <img
+        alt="Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
+              International"
+        style={{
+          float: `left`,
+          margin: `0.5em`,
+          borderWidth: `0`,
+        }}
+        src="https://i.creativecommons.org/l/by/4.0/88x31.png"
+      />
+    </a>
+    All works in this site is licensed under a{" "}
+    <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+      Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International
+    </a>{" "}
+    License.
+  </div>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
