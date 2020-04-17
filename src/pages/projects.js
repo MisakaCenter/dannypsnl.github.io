cdd3d2b7c1bd6bd53cd2b58f95fc3da98fc1fcbf
dannypsnl <dannypsnl@gmail.com>
2020-04-18 01:32:56 +0800

    add star button for each project in projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,41 +2,49 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import SiteLink from "../components/site-link"
+import GitHubButton from "react-github-btn"
 import { graphql } from "gatsby"
 
 export default ({ data }) => {
   const code_projects = [
     {
+      isRepo: true,
       link: `https://github.com/dannypsnl/plt-research`,
       name: `plt-research`,
       description: `A collection of PLT researching`,
     },
     {
-      link: `https://github.com/dannypsnl/elz`,
-      name: `elz`,
-      description: `A polymorphism object-oriented programming language`,
-    },
-    {
+      isRepo: true,
       link: `https://github.com/dannypsnl/xnix`,
       name: `xnix`,
       description: `A unix-like system setting`,
     },
     {
+      isRepo: true,
+      link: `https://github.com/dannypsnl/elz`,
+      name: `elz`,
+      description: `A polymorphism object-oriented programming language`,
+    },
+    {
+      isRepo: true,
       link: `https://github.com/dannypsnl/little-scheme`,
       name: `little-scheme`,
       description: `A little scheme for playing SICP`,
     },
     {
+      isRepo: true,
       link: `https://github.com/dannypsnl/redux`,
       name: `redux`,
       description: `redux in Go/Rust`,
     },
     {
+      isRepo: true,
       link: `https://github.com/dannypsnl/rocket`,
       name: `rocket`,
       description: `A light weight Go web framework`,
     },
     {
+      isRepo: true,
       link: `https://github.com/dannypsnl/on`,
       name: `on`,
       description: `A command line tool execute commands base on a command context to avoid duplicate typing`,
@@ -45,6 +53,7 @@ export default ({ data }) => {
 
   const contributions = [
     {
+      isRepo: true,
       link: `https://github.com/llir/llvm`,
       name: `llir/llvm`,
       description: `Library for interacting with LLVM IR in pure Go`,
@@ -102,10 +111,35 @@ export default ({ data }) => {
 
 const ProjectLi = ({ project }) => (
   <li>
-    <SiteLink url={project.link}>{project.name}</SiteLink>
-    {" : "}
+    <div
+      style={{
+        display: `inline`,
+        marginRight: `0.3em`,
+      }}
+    >
+      <SiteLink url={project.link}>{project.name}</SiteLink>
+    </div>
+    {project.isRepo ? (
+      <div
+        style={{
+          display: `inline`,
+          marginRight: `0.3em`,
+        }}
+      >
+        <GitHubButton
+          href={project.link}
+          data-icon="octicon-star"
+          data-show-count="true"
+          aria-label="Star ntkme/github-buttons on GitHub"
+        >
+          Star
+        </GitHubButton>
+      </div>
+    ) : null}
+    :
     <em
       style={{
+        marginLeft: `0.3em`,
         fontSize: `0.9em`,
       }}
     >
