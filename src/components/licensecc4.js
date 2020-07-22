7899cc3ae40a1765272a960f87131ac46d51c0f7
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-07-22 14:13:26 +0800

    let license CC 4.0 be a component

---
+++ src/components/licensecc4.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+export const LicenseCC4 = () => (
+  <div>
+    <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+      <img
+        alt="Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
+              International"
+        style={{
+          float: `left`,
+          margin: `0.5em`,
+          borderWidth: `0`,
+        }}
+        src="https://i.creativecommons.org/l/by/4.0/88x31.png"
+      />
+    </a>
+    All works in this site is licensed under a{" "}
+    <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+      Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International
+    </a>{" "}
+    License.
+  </div>
+)
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,6 +6,7 @@ import SiteLink from "../components/site-link"
 
 import "../styles/code-block-theme.css"
 import "../styles/code-block-hightlight.css"
+import { LicenseCC4 } from "../components/licensecc4"
 
 export default ({ data }) => {
   const post = data.markdownRemark
@@ -79,28 +80,6 @@ const ShowTag = ({ children }) => (
   >
     {children}
   </em>
-)
-
-const LicenseCC4 = () => (
-  <div>
-    <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
-      <img
-        alt="Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
-              International"
-        style={{
-          float: `left`,
-          margin: `0.5em`,
-          borderWidth: `0`,
-        }}
-        src="https://i.creativecommons.org/l/by/4.0/88x31.png"
-      />
-    </a>
-    All works in this site is licensed under a{" "}
-    <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
-      Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International
-    </a>{" "}
-    License.
-  </div>
 )
 
 export const query = graphql`
