3c4d3a3b40ffd2770a4ce81111abc07d60eb94d9
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-07-22 14:56:46 +0800

    (#22) related articles
    close #22

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -8,6 +8,7 @@ import "../styles/code-block-theme.css"
 import "../styles/code-block-hightlight.css"
 import { LicenseCC4 } from "../components/licensecc4"
 import { BuyMeACoffee } from "../components/buy-me-a-coffe"
+import Header from "../components/header"
 
 export default ({ data }) => {
   const FooterSection = ({ children }) => (
@@ -60,11 +61,64 @@ export default ({ data }) => {
             <ShowTag>{tag}</ShowTag>
           ))}
         </FooterSection>
+        <p />
+        <SimilarArticlesComponent
+          currentArticle={{
+            title: post.frontmatter.title,
+            categories: post.frontmatter.categories,
+            tags: post.frontmatter.tags,
+          }}
+          articles={data.allMarkdownRemark.edges.map((edge) => edge.node)}
+        />
         <BuyMeACoffee />
         <LicenseCC4 />
         <Disqus config={disqusConfig} />
       </div>
     </Layout>
+  )
+}
+
+const difference = (a, b) =>
+  a.filter((x) => !b.includes(x)).concat(b.filter((x) => !a.includes(x)))
+const intersection = (a, b) => a.filter((x) => b.includes(x))
+const same = (a, b) => {
+  return difference(a, b).length === 0
+}
+
+const SimilarArticlesComponent = ({ currentArticle, articles }) => {
+  let shows = articles
+    .filter(
+      (article) =>
+        same(article.frontmatter.categories, currentArticle.categories) &&
+        article.frontmatter.title !== currentArticle.title
+    )
+    .sort((article1, article2) => {
+      let a = intersection(article1.frontmatter.tags, currentArticle.tags)
+      let b = intersection(article2.frontmatter.tags, currentArticle.tags)
+
+      if (b.length > a.length) {
+        return 1
+      } else if (b.length === a.length) {
+        return 0
+      } else {
+        return -1
+      }
+    })
+    .slice(0, 4)
+
+  return (
+    <>
+      <h4>Similar Articles</h4>
+      <section className="similar-articles">
+        <ul>
+          {shows.map((article, i) => (
+            <li key={i}>
+              <a href={article.fields.slug}>{article.frontmatter.title}</a>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
   )
 }
 
@@ -81,6 +135,21 @@ export const query = graphql`
         title
         categories
         tags
+      }
+    }
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            categories
+            tags
+          }
+          fields {
+            slug
+          }
+        }
       }
     }
   }
